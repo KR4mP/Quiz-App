5b93d02d40917f46049d6a42f197c7f05397a62c
Can Ahmet Demirci <cnahmetdmrc@gmail.com>
2021-06-13 12:33:06 +0300

    added questions and answers

---
+++ script.js
@@ -0,0 +1,42 @@
+const quizData = [
+    {
+        question: 'What is the world’s longest river called?',
+        a: 'Amazon',
+        b: 'Nile',
+        c: 'Mississippi',
+        d: 'Dicle',
+        correct: 'b'
+    },
+    {
+        question: 'Which country gifted the Statue of Liberty to the US?',
+        a: 'France',
+        b: 'Turkey',
+        c: 'India',
+        d: 'China',
+        correct: 'a'
+    },
+    {
+        question: 'What does Na stand for on the periodic table?',
+        a: 'Uranium',
+        b: 'Aliminium',
+        c: 'Nambia',
+        d: 'Sodium',
+        correct: 'd'
+    },
+    {
+        question: 'Which company uses Santa Claus for their advertisements?',
+        a: 'Apple',
+        b: 'McDonalds',
+        c: 'Coca-Cola',
+        d: 'Facebook',
+        correct: 'c'
+    },
+    {
+        question: 'What temperature (in Fahrenheit) does water freeze at?',
+        a: '100 degrees',
+        b: '0 degrees',
+        c: '32 degrees',
+        d: '54 degrees',
+        correct: 'c'
+    }
+]
